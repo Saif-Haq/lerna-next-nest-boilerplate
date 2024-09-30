549918027d9ac8b638c682f32a4ebe1dfa513556
Saif-Haq <saifhaq01@gmail.com>
2024-09-30 11:38:01 +0500

    latest changes

--- packages/frontend-a/tailwind.config.ts
+++ packages/frontend-a/tailwind.config.ts
@@ -1,19 +1,36 @@
 import type { Config } from "tailwindcss";
 
+// const config: Config = {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: "var(--background)",
+//         foreground: "var(--foreground)",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+// export default config;
+
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+import themeConfig from "../shared/lib/config/tailwind/tailwind.config";
+// import themeConfig from "shared";
+
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "../shared/lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+  theme: themeConfig.theme,
   plugins: [],
 };
+
 export default config;
